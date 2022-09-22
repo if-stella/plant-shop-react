cac61ee411ef0230308024f24bbae79ded006c68
Stella Raab <hallo@stellaraab.de>
2022-09-22 20:29:35 +0200

    Added product-cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Headlinecards from './components/Headlinecards';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
+      <Headlinecards />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,33 +3,31 @@ import { AiOutlineMenu, AiOutlineSearch, AiOutlineTags } from 'react-icons/ai';
 import { BsFillCartFill } from 'react-icons/bs';
 import {TbTruckDelivery} from 'react-icons/tb';
 import { MdOutlineContactSupport} from 'react-icons/md';
-import {RiPlantFill} from 'react-icons/ri';
 import {BiWallet, BiHeart} from 'react-icons/bi';
 import {GrFormClose} from 'react-icons/gr';
+
+import {ReactComponent as PlantIcon} from '../assets/littleleave.svg';
 
 const Navbar = () => {
 const [nav, setNav] = useState(false)
 
   return (
-    <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
+    <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4 fixed z-[5] w-full'>
       {/* Left side */}
       <div className='flex items-center'>
         <div onClick={()=> setNav(!nav)} className='cursor-pointer'>
-          <AiOutlineMenu size={25} />
+          <AiOutlineMenu size={25} className="text-[#BAD3D3]"/>
         </div>
         <div className="flex items-center gap-2 px-4">
-        <RiPlantFill size={25}/>
-        <h1 className='text-1xl sm:text-2xl lg:text-3xl'>Best plants</h1>
+        <PlantIcon className="h-8"/>
+        <h1 className='sm:text-1xl lg:text-2xl text-[#558A76]'>Little leaf</h1>
         </div>
-        <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
-          <p className='bg-black text-white rounded-full px-2 py-1'>Delivery</p>
-          <p className='px-2 py-1'>Pickup</p>
-        </div>
+
       </div>
 
       {/* Search Input */}
-      <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
-        <AiOutlineSearch size={25} />
+      <div className='bg-[#BAD3D3] rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
+        <AiOutlineSearch size={25} className="text-[#558A76]" />
         <input
           className='bg-transparent p-2 w-full focus:outline-none'
           type='text'
@@ -37,7 +35,7 @@ const [nav, setNav] = useState(false)
         />
       </div>
       {/* Cart button */}
-      <button className='bg-black text-white hidden md:flex items-center py-2 rounded-full'>
+      <button className='bg-[#558A76] border-[#558A76] text-white hidden md:flex items-center py-2 rounded-full'>
         <BsFillCartFill size={20} className='mr-2' /> Cart
       </button>
 
@@ -54,16 +52,16 @@ const [nav, setNav] = useState(false)
           className='absolute right-4 top-4 cursor-pointer'
         />
         <div className="flex items-center gap-2 p-4">
-        <RiPlantFill size={20}/>
-        <h2>Best plants</h2>
+        <PlantIcon className="h-6"/>
+        <h2 className="text-[#558A76]">Little leaf</h2>
         </div>
         <nav>
-            <ul className='flex flex-col p-4 text-gray-800'>
-                <li className='text-xl pb-4 flex'><TbTruckDelivery size={25} className='mr-4' /> Orders</li>
-                <li className='text-xl py-4 flex'><BiHeart size={25} className='mr-4' /> Favorites</li>
-                <li className='text-xl py-4 flex'><BiWallet size={25} className='mr-4' /> Wallet</li>
-                <li className='text-xl py-4 flex'><MdOutlineContactSupport size={25} className='mr-4' /> Help</li>
-                <li className='text-xl pt-4 flex'><AiOutlineTags size={25} className='mr-4' /> Promotions</li>
+            <ul className='flex flex-col p-4'>
+                <li className='text-xl pb-4 flex'><TbTruckDelivery size={25} className='mr-4 text-[#558A76]' /> Orders</li>
+                <li className='text-xl py-4 flex'><BiHeart size={25} className='mr-4 text-[#558A76]' /> Favorites</li>
+                <li className='text-xl py-4 flex'><BiWallet size={25} className='mr-4 text-[#558A76]' /> Wallet</li>
+                <li className='text-xl py-4 flex'><MdOutlineContactSupport size={25} className='mr-4 text-[#558A76]' /> Help</li>
+                <li className='text-xl pt-4 flex'><AiOutlineTags size={25} className='mr-4 text-[#558A76]' /> Promotions</li>
             </ul>
         </nav>
       </div>
